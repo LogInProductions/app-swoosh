aa9e139a2440b9f3596a927c222aa259e2e4c225
Logan Skurdal <loganmacos@Logans-Mac.local>
2018-03-19 14:07:29 -0600

    less 5 changes

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Logan Skurdal on 3/18/18.
+//  Copyright © 2018 Logan Skurdal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // swoosh.frame = CGRect(x: view.frame.size.width/2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        
+        // bgImg.frame = view.frame;
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
